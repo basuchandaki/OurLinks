04cec67ad9bd632a1d5f4cd6021fb60a353f95cf
basuchandaki <basavaraj.chandaki@centurylink.com>
2016-05-23 11:23:50 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+
+function emp()
+{
+var emp=["Sonoo","Vimal","Ratan"];  
+for (i=0;i<emp.length;i++){  
+document.write(emp[i] + "<br/>");  
+}  
+}
+
+function emp2()
+{
+var string=new String("alice in wonderland ");
+document.write(string);
+
+var emp=new Array();
+emp[0]="basu";
+emp[1]="chandaki";
+emp[2]="vijayanagar";
+for(i=0;i<emp.length;i++)
+{
+document.write(emp[i]+"<br>");
+}
+}
